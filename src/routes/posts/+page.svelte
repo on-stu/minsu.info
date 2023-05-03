bc6a30e4a254f87203cdc8165013ca46489b0e56
on-stu <minsu0523@naver.com>
2023-05-03 23:19:24 +0900

    blog scroll animation

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
+	import ScrollDetectContainer from '$lib/components/ScrollDetectContainer.svelte';
+	import { fadeFromBottom } from '$lib/utils/scrollDetectionStyle';
 	import Banner from './Banner.svelte';
 	import PostCard from './PostCard.svelte';
-
+	let isInView: boolean;
 	let posts = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 </script>
 
 <Banner />
-<Container>
-	<div class="mt-4 p-4">
-		<h2 class="text-2xl font-bold text-neutral-50">최신글 보기</h2>
-	</div>
-	<div class="grid grid-cols-3 gap-x-4 gap-y-8 p-4 max-md:grid-cols-1">
-		{#each posts as post}
-			<PostCard />
-		{/each}
-	</div>
-</Container>
+<ScrollDetectContainer bind:isInView>
+	<Container>
+		<div class="mt-4 p-4">
+			<h2 class="text-2xl font-bold text-neutral-50 {fadeFromBottom(isInView, 100)}">
+				최신글 보기
+			</h2>
+		</div>
+		<div
+			class="grid grid-cols-3 gap-x-4 gap-y-8 p-4 max-md:grid-cols-1 {fadeFromBottom(
+				isInView,
+				100
+			)}"
+		>
+			{#each posts as post}
+				<PostCard />
+			{/each}
+		</div>
+	</Container>
+</ScrollDetectContainer>
--- src/routes/posts/Banner.svelte
+++ src/routes/posts/Banner.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
+	import ScrollDetectContainer from '$lib/components/ScrollDetectContainer.svelte';
+	import { fadeFromBottom } from '$lib/utils/scrollDetectionStyle';
 	import { Icon, Search } from 'svelte-hero-icons';
+	let isInView: boolean;
 </script>
 
-<div class="relative">
-	<video
-		src="/videos/stars.mp4"
-		autoplay
-		loop
-		muted
-		playsinline
-		class="h-[500px] w-full object-cover object-bottom"
-	/>
-	<div class=" h-[100px] w-full bg-gradient-to-b from-black to-neutral-900" />
-	<div class="absolute inset-0 flex flex-col bg-gradient-to-b from-transparent to-neutral-900">
-		<div class="flex h-full flex-col items-center justify-center gap-2 max-md:px-4">
-			<h1 class="text-4xl font-bold text-white">Welcome to my blog</h1>
-			<p class="text-white">This is a blog about my journey as a developer</p>
-			<span
-				class="mt-4 flex w-full max-w-4xl items-center gap-x-2 rounded-2xl bg-neutral-50 px-4 outline-none"
-			>
-				<input class="h-full w-full bg-transparent p-4" placeholder="검색어를 입력해주세요" />
-				<Icon src={Search} class="h-6 w-6 text-neutral-400" />
-			</span>
+<ScrollDetectContainer bind:isInView>
+	<div class="relative">
+		<video
+			src="/videos/stars.mp4"
+			autoplay
+			loop
+			muted
+			playsinline
+			class="h-[500px] w-full object-cover object-bottom"
+		/>
+		<div class=" h-[100px] w-full bg-gradient-to-b from-black to-neutral-900" />
+		<div class="absolute inset-0 flex flex-col bg-gradient-to-b from-transparent to-neutral-900">
+			<div class="flex h-full flex-col items-center justify-center gap-2 max-md:px-4">
+				<h1 class="text-4xl font-bold text-neutral-50 {fadeFromBottom(isInView)}">
+					Welcome to my blog
+				</h1>
+				<p class="text-neutral-50 {fadeFromBottom(isInView)}">
+					This is a blog about my journey as a developer
+				</p>
+				<span
+					class="mt-4 flex w-full max-w-4xl items-center gap-x-2 rounded-2xl bg-neutral-50 px-4 outline-none {fadeFromBottom(
+						isInView
+					)}"
+				>
+					<input class="h-full w-full bg-transparent p-4" placeholder="검색어를 입력해주세요" />
+					<Icon src={Search} class="h-6 w-6 text-neutral-400" />
+				</span>
+			</div>
 		</div>
 	</div>
-</div>
+</ScrollDetectContainer>
