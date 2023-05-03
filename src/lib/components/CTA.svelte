9e275b575647331213f2b5f604e781cb90cb63ff
on-stu <minsu0523@naver.com>
2023-05-03 15:56:12 +0900

    FEAT : add mainpage components

---
+++ src/lib/components/CTA.svelte
@@ -0,0 +1,7 @@
+<a
+	href="https://open.kakao.com/o/sK9sDrif"
+	target="_blank"
+	class="mt-[200px] bg-green-800 w-[120px] h-[120px] rounded-full transition-all hover:scale-110 flex justify-center items-center shadow-2xl"
+>
+	<span class="font-bold text-lg text-neutral-100">Contact Me</span>
+</a>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<header class="w-full fixed top-0 left-0 flex text-xl text-neutral-50 justify-between p-10 z-20">
+	<span>
+		<a href="/">Minsu Kim</a>
+	</span>
+	<span class="flex gap-x-4">
+		<a href="/career">Career</a>
+		<a href="/posts">Blog</a>
+		<a href="/projects">Projects</a>
+		<a href="/about">About</a>
+	</span>
+</header>
