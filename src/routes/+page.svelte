c1d2884c00ddd8ec040a6b4e6b9f70fc43a8216d
on-stu <minsu0523@naver.com>
2023-05-03 15:56:32 +0900

    FEAT : main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Cta from '$lib/components/CTA.svelte';
+	import Stacks from './Stacks.svelte';
+</script>
+
+<div class="flex h-screen relative pb-[120px]">
+	<div class="bg-neutral-600 h-full w-1/2 flex justify-center items-center text-neutral-50">
+		<div class="w-2/3 flex flex-col gap-y-4">
+			<h2 class="font-bold text-6xl mb-4">풀스택 개발자</h2>
+			<h1 class=" font-extrabold text-8xl mb-8">김민수</h1>
+			<span class="text-neutral-300 font-medium text-lg">// ABOUT ME</span>
+			<p class="font-bold text-lg">안녕하세요. 코딩을 사랑하는 개발자입니다.</p>
+			<span class="text-neutral-300 font-medium text-lg">
+				문과적 소양과 이과적 능력을 겸비한 '융합형 인재'입니다.
+			</span>
+		</div>
+	</div>
+	<div class="h-full flex w-1/2 justify-center items-end bg-neutral-800">
+		<img src="/images/profile.png" alt="profile" class="w-1/2 object-contain" />
+	</div>
+	<div class="absolute inset-0 flex items-center justify-center">
+		<Cta />
+	</div>
+	<div
+		class="absolute h-[120px] bottom-0 bg-neutral-900 w-full flex justify-around flex-row items-center"
+	>
+		<Stacks />
+	</div>
+</div>
