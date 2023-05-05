6750d884fedf8380416efa2ab7fbab1412e44ce6
on-stu <minsu0523@naver.com>
2023-05-05 11:10:06 +0900

    careers done

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,1 +1,44 @@
-this is career page
+<script lang="ts">
+	import Container from '$lib/components/Container.svelte';
+	import FloatingButton from '$lib/components/FloatingButton.svelte';
+	import ScrollDetectContainer from '$lib/components/ScrollDetectContainer.svelte';
+	import { careers } from '$lib/constants/careers';
+	import { fadeFromBottom } from '$lib/utils/scrollDetectionStyle';
+	import CareerCard from './CareerCard.svelte';
+	let isInView: boolean;
+
+	let delayStyles = [
+		'delay-300',
+		'delay-500',
+		'delay-700',
+		'delay-1000',
+		'delay-[1200ms]',
+		'delay-[1400ms]',
+	];
+</script>
+
+<div class="relative w-screen">
+	<ScrollDetectContainer bind:isInView>
+		<Container class="relative mt-[100px] flex min-h-screen flex-col text-neutral-50">
+			{#each careers as career, i}
+				<div
+					class="flex w-full {i % 2 === 0 ? 'justify-start' : 'justify-end'} {fadeFromBottom(
+						isInView
+					)} {delayStyles[i]}"
+				>
+					<div class="w-1/2 p-4 max-md:w-full">
+						<CareerCard isLeft={i % 2 === 0} {...career} />
+					</div>
+				</div>
+			{/each}
+			<div class="absolute inset-0 flex h-full w-full justify-center max-md:justify-start">
+				<div
+					class="{isInView
+						? 'h-full'
+						: 'h-0'} w-[2px] rounded-full bg-green-800 transition-all duration-500 ease-in-out"
+				/>
+			</div>
+		</Container>
+	</ScrollDetectContainer>
+	<FloatingButton />
+</div>
---
+++ src/routes/careers/CareerCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let startDate: string = '2021.01';
+	export let endDate: string = '현재';
+	export let isLeft: boolean = false;
+	export let title: string = 'Software Engineer';
+	export let description: string =
+		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus quaerat vitae quidem odit ipsam nemo repudiandae culpa, perspiciatis non suscipit laborum! Quae animi doloremque cumque reiciendis, voluptate doloribus quod ipsam!';
+</script>
+
+<div
+	class="w-full rounded-2xl {isLeft
+		? 'max-md:rounded-bl-none md:rounded-br-none'
+		: 'rounded-bl-none'} bg-neutral-300 p-4"
+>
+	<h1 class="font-medium text-green-800">{startDate + ' ~ ' + endDate}</h1>
+	<h2 class="font-bold text-green-800">{title}</h2>
+	<p class="text-neutral-500">
+		{description}
+	</p>
+</div>
